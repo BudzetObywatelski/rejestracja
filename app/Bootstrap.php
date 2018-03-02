d6a427d7d504af7033b6a3a83afb8534993d8b05
Amadeusz <a.dzieciol@citycore.pro>
2018-03-02 16:00:16 +0100

    usuniecie bazy z bootstrap, zmian nazwy sesji

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -36,24 +36,7 @@ class Bootstrap
     
     public function __construct()
     {
-        try {
-            if(!empty(DB_HOST)) {
-                $dbConfig = array(
-                    'host' => DB_HOST, 
-                    'dbname' => DB_DATABASE, 
-                    'username' => DB_USER, 
-                    'password' => DB_PASS,
-                );
-                $this->db = new Database($dbConfig);
-                $this->db->setErrorLog(setErrorLog); // Debugowanie
-            }
-
-        } catch(DBException $e) {
-            echo 'The connect can not create: ' . $e->getMessage();
-            exit();
-        }
-
-        $this->session  = new Session('session_name'); // Best to set projectName
+        $this->session  = new Session('bo_rejestracja'); // Best to set projectName
         $this->msg = new Messages($this->session);     // Default notify class
         $this->token  = new Token($this->session);     // Default csrf token
 
