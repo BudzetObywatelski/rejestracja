2a7a75b8d98bfddbbfbbc07bf5e095b36bf67ddd
Amadeusz <a.dzieciol@citycore.pro>
2018-03-08 15:03:39 +0000

    poprawki

--- app/View/templates/users/index.html.php
+++ app/View/templates/users/index.html.php
@@ -36,6 +36,9 @@
 													<h5 class="text-center"><b>Serdecznie zapraszamy do udziału w&nbsp;panelu obywatelskim<br/> „Co zrobić aby oddychać czystym powietrzem w&nbsp;Lublinie?”</b></h5>
 
 													<p>Jeżeli zdecydował/a się Pan/Pani na wzięcie udziału w&nbsp;panelu i&nbsp;zgłoszenie się do ostatecznego losowania, prosimy o&nbsp;przygotowanie otrzymanego pocztą zaproszenia wraz z&nbsp;kodem identyfikacyjnym znajdującym się w&nbsp;prawym górnym rogu.  
+													<br/><br/>
+													Rejestracja przez Internet jest otwarta od <span class="date">{$registerConfig.start}</span>, do <span class="date">{$registerConfig.end}</span>, do godziny 23:59.<br/><br/>
+													Więcej informacji o&nbsp;panelu obywatelskim w&nbsp;Lublinie jest dostępnych na <a href="www.lublin.eu/panel">www.lublin.eu/panel</a>.<br/><br/>
 													</p>
 												</div>
 			                            	</div>
